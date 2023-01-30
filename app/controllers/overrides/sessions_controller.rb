72df81067e36d970ee6767a924a7dc66174a99b4
samwelkinuthia <samwuelkinuthia@gmail.com>
2023-01-30 08:09:40 +0300

    reset login override

--- app/controllers/overrides/sessions_controller.rb
+++ app/controllers/overrides/sessions_controller.rb
@@ -1,9 +1,9 @@
 module Overrides
   class SessionsController < DeviseTokenAuth::SessionsController
 
-    protected
-    def render_create_success
-      render json: @resource
-    end
+    # protected
+    # def render_create_success
+    #   render json: @resource
+    # end
   end
 end
