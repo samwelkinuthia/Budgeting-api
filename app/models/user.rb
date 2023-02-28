0d59c838f128fe23a76d4e36d489b0c0972286ba
samwelkinuthia <samwuelkinuthia@gmail.com>
2023-02-28 18:33:42 +0300

    update user type to default 1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,19 @@
 # frozen_string_literal: true
 
 class User < ActiveRecord::Base
-
+  after_validation :update_params
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
+
+  def update_params
+    return unless user_type_id.nil?
+
+    update(user_type_id: 1, county_id: nil)
+  end
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   # validates :size, inclusion: { in: %w(small medium large),
   #                               message: "%{value} is not a valid size" }
-
 end
-
-
