9cab6d55b2c85856207c65c037a849601f7bec67
samwelkinuthia <samwuelkinuthia@gmail.com>
2023-02-28 18:32:58 +0300

    route registration controller to override

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   get 'reports', to: 'reports#index'
   get 'users', to: 'users#index'
   get 'users/:id', to: 'users#show'
   get 'usertypes', to: 'users#types'
   put 'users/:id', to: 'users#update'
-  post 'county_budgets/:countyId/:fiscalYearId', to: "county_budgets#create"
+  post 'county_budgets/:countyId/:fiscalYearId', to: 'county_budgets#create'
   resources :revenue_sources
   resources :fiscal_years
   resources :counties do
@@ -15,7 +17,8 @@ Rails.application.routes.draw do
     end
   end
   mount_devise_token_auth_for 'User', at: 'auth', controllers: {
-    sessions:  'overrides/sessions',
+    sessions: 'overrides/sessions',
+    registrations: 'overrides/registrations'
   }
   # mount_devise_token_auth_for 'User', at: 'auth'
 end
