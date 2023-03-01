c83c4fa529a86ba147d53dc0a2d2ad98137afad5
samwelkinuthia <samwuelkinuthia@gmail.com>
2023-03-01 18:25:19 +0300

    update readme and seeds.rb

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Budgeting API
+
 API to track budgetary spending on a county and national level.
+
 ## Install
 
 ### Clone the repository
@@ -15,12 +17,10 @@ cd project
 ruby -v
 ```
 
-The ouput should start with something like `ruby 3.0.0`
-
 If not, install the right ruby version using [rbenv](https://github.com/rbenv/rbenv) (it could take a while):
 
 ```shell
-rbenv install 3.0.0
+rbenv install 3.2.0
 ```
 
 ### Install dependencies
@@ -31,19 +31,21 @@ Using [Bundler](https://github.com/bundler/bundler)
 bundle
 ```
 
-<!-- ### Set environment variables
-
-Using [Figaro](https://github.com/laserlemon/figaro):
-
-See [config/application.yml.sample](https://github.com/juliendargelos/project/blob/master/config/application.yml.sample) and contact the developer: [contact@juliendargelos.com](mailto:contact@juliendargelos.com) (sensitive data). -->
-
 ### Initialize the database
-
+You can customize the seeded user data by editing config/db/seeds.rb
 ```shell
-rails db:create db:migrate db:seed
+rails db:create
+rails db:migrate 
+rails db:seed
 ```
+
 ## Serve
 
 ```shell
 rails s
 ```
+
+### Documentation
+- Navigate to ```http://localhost:3000/api-docs/index.html``` for swagger api documentation.
+- Headers required: client, uid, access_token.
+- Authentication framework used is [Devise Token Auth](https://devise-token-auth.gitbook.io/devise-token-auth/).
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,78 +1,44 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-
-
-UserType.create(name: "SystemAdmin")
-UserType.create(name: "CountyAdmin")
-UserType.create(name: "DepartmentAdmin")
+UserType.create(name: 'SystemAdmin')
+UserType.create(name: 'CountyAdmin')
+UserType.create(name: 'DepartmentAdmin')
 counties = JSON.parse("[{\"name\":\"Mombasa\",\"code\":1},{\"name\":\"Kwale\",\"code\":2},{\"name\":\"Kilifi\",\"code\":3},{\"name\":\"Tana River\",\"code\":4},{\"name\":\"Lamu\",\"code\":5},{\"name\":\"Taita-Taveta\",\"code\":6},{\"name\":\"Garissa\",\"code\":7},{\"name\":\"Wajir\",\"code\":8},{\"name\":\"Mandera\",\"code\":9},{\"name\":\"Marsabit\",\"code\":10},{\"name\":\"Isiolo\",\"code\":11},{\"name\":\"Meru\",\"code\":12},{\"name\":\"Tharaka-Nithi\",\"code\":13},{\"name\":\"Embu\",\"code\":14},{\"name\":\"Kitui\",\"code\":15},{\"name\":\"Machakos\",\"code\":16},{\"name\":\"Makueni\",\"code\":17},{\"name\":\"Nyandarua\",\"code\":18},{\"name\":\"Nyeri\",\"code\":19},{\"name\":\"Kirinyaga\",\"code\":20},{\"name\":\"Murang'a\",\"code\":21},{\"name\":\"Kiambu\",\"code\":22},{\"name\":\"Turkana\",\"code\":23},{\"name\":\"West Pokot\",\"code\":24},{\"name\":\"Samburu\",\"code\":25},{\"name\":\"Trans-Nzoia\",\"code\":26},{\"name\":\"Uasin Gishu\",\"code\":27},{\"name\":\"Elgeyo-Marakwet\",\"code\":28},{\"name\":\"Nandi\",\"code\":29},{\"name\":\"Baringo\",\"code\":30},{\"name\":\"Laikipia\",\"code\":31},{\"name\":\"Nakuru\",\"code\":32},{\"name\":\"Narok\",\"code\":33},{\"name\":\"Kajiado\",\"code\":34},{\"name\":\"Kericho\",\"code\":35},{\"name\":\"Bomet\",\"code\":36},{\"name\":\"Kakamega\",\"code\":37},{\"name\":\"Vihiga\",\"code\":38},{\"name\":\"Bungoma\",\"code\":39},{\"name\":\"Busia\",\"code\":40},{\"name\":\"Siaya\",\"code\":41},{\"name\":\"Kisumu\",\"code\":42},{\"name\":\"Homa Bay\",\"code\":43},{\"name\":\"Migori\",\"code\":44},{\"name\":\"Kisii\",\"code\":45},{\"name\":\"Nyamira\",\"code\":46},{\"name\":\"Nairobi\",\"code\":47}]")
-
-
-# User.all.add_role(:System_Admin)
-
-# 3.times do
-#   FiscalYear.create!(name: "#{rand(1950..2010).to_s + "-" + rand(2011..2050).to_s}", date_from: Faker::Date.forward(days: 1).to_datetime, date_to: Faker::Date.forward(days: 365).to_datetime)
-# end
-# 10.times do
-#   County.create!(counties.sample)
-# end
 
 counties[1..10].each do |item|
   County.create!(item)
 end
 
 %w[2022-2023 2023-2024].each do |item|
-  FiscalYear.create!({name:item, date_from: Faker::Date.forward(days: 1).to_datetime, date_to: Faker::Date.forward(days: 365).to_datetime})
+  FiscalYear.create!({ name: item, date_from: Faker::Date.forward(days: 1).to_datetime,
+                       date_to: Faker::Date.forward(days: 365).to_datetime })
 end
-
-# [
-#   "Health",
-#   "Lands",
-#   "Trade",
-#   "Education, Sports & ICT",
-#   "Gender",
-#   "Transport",
-#   "Agriculture",
-#   "Water",
-#   "Finance",
-#   "GVN CS"
-# ].each do | item|
-#   x = {name:item, totalBudget:rand(10000000..100000000).to_d, developmentBudget:rand(1000000..10000000).to_d, recurrentBudget:rand(1000000..10000000).to_d, pendingBills:rand(1000000..10000000).to_d, county_id: 1}
-#   Department.create!(x)
-# end
 
 [
-  "National Government",
-  "Local Revenue",
-  "Exchequer",
-  "Balance Brought Forward",
-  "Conditional Grants"
-].each do | item|
-  RevenueSource.create!({name: item})
+  'National Government',
+  'Local Revenue',
+  'Exchequer',
+  'Balance Brought Forward',
+  'Conditional Grants'
+].each do |item|
+  RevenueSource.create!({ name: item })
 end
 
+projects = []
+County.all.each do |county|
+  CountyBudget.create!({ "fiscal_year_id": 1, "county_id": county.id, "totalBudget": rand(1_000_000..9_999_999) })
 
-elapsed = Benchmark.measure do
-  projects = []
-  County.all.each do |county|
-    CountyBudget.create!({"fiscal_year_id": 1, "county_id": county.id,"totalBudget": rand(1000000..9999999)})
-
-    rand(5..10).times do |item|
-      department = Department.create(county_id: county.id, name: "#{county.name} - #{Faker::Artist.name} Department - #{rand(0..5656)}", totalBudget: rand(1000000..9999999).to_d, developmentBudget: rand(1000000..9999999).to_d, recurrentBudget: rand(1000000..9999999).to_d, pendingBills: rand(1000000..9999999).to_d)
-      rand(1..5).times do |proj|
-        dept = department.projects.new(title: "#{Faker::Movies::StarWars.character} Project #{rand(999..1009992)}", description: Faker::Quote.famous_last_words, revenue_source_id: rand(1..3), department_id: Department.last.id, ward: Faker::Address.state, location: Faker::Address.state, status: %w[New OnGoing Complete Paused].sample, budgetAmount: rand(1000000..9999999).to_d, spentAmount: rand(100000..500000).to_d, fiscal_year_id: 1)
-        projects.push(dept)
-      end
+  rand(5..10).times do |_item|
+    department = Department.create(county_id: county.id,
+                                   name: "#{county.name} - #{Faker::Artist.name} Department - #{rand(0..5656)}", totalBudget: rand(1_000_000..9_999_999).to_d, developmentBudget: rand(1_000_000..9_999_999).to_d, recurrentBudget: rand(1_000_000..9_999_999).to_d, pendingBills: rand(1_000_000..9_999_999).to_d)
+    rand(1..5).times do |_proj|
+      dept = department.projects.new(title: "#{Faker::Movies::StarWars.character} Project #{rand(999..1_009_992)}",
+                                     description: Faker::Quote.famous_last_words, revenue_source_id: rand(1..3), department_id: Department.last.id, ward: Faker::Address.state, location: Faker::Address.state, status: %w[New OnGoing Complete Paused].sample, budgetAmount: rand(1_000_000..9_999_999).to_d, spentAmount: rand(100_000..500_000).to_d, fiscal_year_id: 1)
+      projects.push(dept)
     end
   end
-  Project.import(projects)
 end
+Project.import(projects)
 
-User.create!(email: "sam@sam.com", password: "samsam", password_confirmation: "samsam", county_id: 1, department_id: 1, user_type_id: 1)
+User.create!(email: 'user@user.com', password: 'useruser', password_confirmation: 'useruser', county_id: 1, department_id: 1,
+             user_type_id: 1)
 
 puts "Elapsed time is #{elapsed.real} seconds"
